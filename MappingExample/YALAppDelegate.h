5d0e688586075cf61fbac9edc13ca1d41703ffa3
Shemet <shemet.dmitriy@gmail.com>
2014-03-11 19:02:05 +0200

    Init

---
+++ MappingExample/YALAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YALAppDelegate.h
+//  MappingExample
+//
+//  Created by dshe on 3/11/14.
+//  Copyright (c) 2014 Yalantis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YALAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MappingExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MappingExample
+//
+//  Created by dshe on 3/11/14.
+//  Copyright (c) 2014 Yalantis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YALAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YALAppDelegate class]));
+    }
+}
